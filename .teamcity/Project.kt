923828d68649d1e66076fd8252821abd9167e742
Repository with test projects <teamcity.qa.server@gmail.com>
2023-06-06 12:51:14 +0200

    Update Project.kt

--- .teamcity/Project.kt
+++ .teamcity/Project.kt
@@ -3,6 +3,11 @@ import jetbrains.buildServer.configs.kotlin.v2023_05.vcs.GitVcsRoot
 
 object Project : Project({
     vcsRoot(TestVcsRoot)
+    buildType(Build)
+})
+
+object Build : BuildType({
+    name = "Build"
 })
 
 object TestVcsRoot : GitVcsRoot({
