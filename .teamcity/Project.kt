aaf7d89b86418dad939cd8516da74947f874b5f8
Repository with test projects <teamcity.qa.server@gmail.com>
2023-06-06 13:50:31 +0200

    Update Project.kt

--- .teamcity/Project.kt
+++ .teamcity/Project.kt
@@ -11,6 +11,7 @@ object Build : BuildType({
 })
 
 object TestVcsRoot : GitVcsRoot({
+    id("TestRoot"))
     name = "Test Root"
     url = "https://github.com/tcqa-repos/TestKotlinSettings"
     branch = "main"
